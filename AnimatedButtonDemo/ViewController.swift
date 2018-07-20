dc35d22ceee74a10962834fe8cf418b7b15f5395
Michael Bernat <michael@hibernat.com>
2018-07-20 17:35:50 +0200

    Initial Commit

---
+++ AnimatedButtonDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedButtonDemo
+//
+//  Created by Michael Bernat on 20/07/2018.
+//  Copyright © 2018 Michael Bernat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
